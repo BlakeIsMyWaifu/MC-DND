4434c8a2b24980ae97a434ba0349f5d8540455cc
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2023-11-02 11:27:31 +0000

    :sparkles: logs

--- src/main/java/dev/blakeismywaifu/mcdnd/Main.java
+++ src/main/java/dev/blakeismywaifu/mcdnd/Main.java
@@ -1,14 +1,17 @@
 package dev.blakeismywaifu.mcdnd;
 
+import dev.blakeismywaifu.mcdnd.Utils.Log;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
+		Log.info("Plugin Enabled");
 	}
 
 	@Override
 	public void onDisable() {
+		Log.info("Plugin Disabled");
 	}
 }
